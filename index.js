71d968afe9122f9612f2d4fcb7a5974b8b44d5ac
mthomas3 <thomas.martins.dev@gmail.com>
2020-04-21 19:18:13 +0200

    [DISPLAY] home

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("[*]_[*] What are you doing here?! [*]_[*]")
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Thomas Martins')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('Tech lead iOS ')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('(Swift) -> lover, <br/> developer and supporter.')
+    .start();
